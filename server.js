cb691d2b88c841929fa807a2067c7c2969bd275a
linho000 <149630614+linho000@users.noreply.github.com>
2023-11-23 15:34:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
+
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const io = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
